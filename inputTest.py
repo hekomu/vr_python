f09c9466ae228749d0f4e536b46ccadc42c463ea
soohyun.myung <soohyun040621@gmail.com>
2025-03-17 11:52:35 +0900

    변수수

---
+++ inputTest.py
@@ -0,0 +1,4 @@
+num1=int(input("숫자1 ===>"))
+num2=int(input("숫자2 ===>"))
+result=num1+num2
+print(num1,"+",num2,"=",result)
---
+++ 변수.py
@@ -0,0 +1,12 @@
+num1=100
+num2=200
+numSum=num1+num2
+print(num1,"+",num2,"=",numSum)
+
+
+str1="Hello"
+str2="World"
+
+print(str1+" "+str2)
+
+
