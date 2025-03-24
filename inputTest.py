383ce16b933289f6b8ed7b65df06238d11790766
soohyun.myung <soohyun040621@gmail.com>
2025-03-24 10:04:37 +0900

    3주차

--- inputTest.py
+++ inputTest.py
@@ -1,4 +1,9 @@
-num1=int(input("숫자1 ===>"))
-num2=int(input("숫자2 ===>"))
-result=num1+num2
-print(num1,"+",num2,"=",result)
+num1=int(input("숫자1 ==> "))
+num2=int(input("숫자2 ==> "))
+
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
+print(num1,"%",num2,"=",num1%num2)
+print(num1,"**",num2,"=",num1**num2)
