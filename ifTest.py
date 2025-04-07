5b857c1f60a3b43751c28546f29bfb990c824c66
soohyun.myung <soohyun040621@gmail.com>
2025-04-07 11:36:44 +0900

    6주차

---
+++ ifTest.py
@@ -0,0 +1,33 @@
+"""
+num=100
+if num>100:
+    print("100보다 ")
+    print("큰 수 입니다.")
+else:
+    print("아닌디요")
+    print("거짓입니다.")
+
+
+
+print("프로그램 종료")
+
+
+num=int(input("숫자 입력  >>>"))
+
+if num % 2 ==0 :
+    print("짝수입니다.")
+else:
+    print("홀수입니다.")
+    
+"""
+
+num=int(input("숫자 입력  >>>"))
+
+if num<1000:
+    if num< 100:
+       print("100보다 작음")
+    else:
+        print("100보다 크고 1000보다 작음")
+else:
+    print("1000보다 큼")
+        
