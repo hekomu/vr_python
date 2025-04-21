564dacc50f69640b78dac0e917fa1833b3064225
soohyun.myung <soohyun040621@gmail.com>
2025-04-21 10:55:44 +0900

    7주차

---
+++ mid.py
@@ -0,0 +1,31 @@
+"""
+num1=int(input("숫자 1 입력==>"))
+num2=int(input("숫자 2 입력==>"))
+
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
+
+-
+name=input("받는 사람:")
+juso=input("주소:")
+weight=int(input("무게(g):"))
+
+print("받는 사람==>",name)
+print("주소 ==>",juso)
+print("배송비 ==>",weight*5,"원")
+
+#gui 문제!!
+
+
+name=input("원본 문자열==>")
+n=len(name) #반복문 문자열
+print("반대 문자열==>",name[3]+name[2]+name[1]+name[0])
+
+"""
+age=int(input("나이를 입력 ==>"))
+if age>19:
+    print("집에 갈 시간이에유")
+
+print("협조 감사합니다.")
